c5e8fce84d21fd98bae7d75a9fef7fd2e99cc973
kevinsahib <191242@ids.upchiapas.edu.mx>
2020-11-09 04:37:39 -0600

    edit .gitinore

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -173,3 +173,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from TrayectoriaBack.local_settings import *
+
+except ImportError:
+    pass
